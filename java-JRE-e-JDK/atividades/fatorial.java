5207e0f3b80cf8d099634350a4e4485c6e566297
GuiK9 <guilherme.tec1107@gmail.com>
2022-09-13 22:37:47 -0300

    classes

---
+++ java-JRE-e-JDK/atividades/fatorial.java
@@ -0,0 +1,11 @@
+public class fatorial {
+
+    public static void main(String[] args) {
+        int fatorial = 1;
+        for (int n = 1; n <= 4; n++) {
+            fatorial *= n;
+        }
+
+        System.out.println(fatorial);
+    }
+}
