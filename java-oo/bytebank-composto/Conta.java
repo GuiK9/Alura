4020f7278b22ed6edadf2986e0bf955ecf868ac5
GuiK9 <guilherme.tec1107@gmail.com>
2022-09-16 16:47:04 -0300

    classes

--- java-oo/bytebank-composto/Conta.java
+++ java-oo/bytebank-composto/Conta.java
@@ -2,7 +2,7 @@ public class Conta {
     double saldo;
     int agencia;
     int numero;
-    String titular;
+    Cliente  titular;
 
     public void deposita(double valor) {
         saldo += valor;
@@ -29,4 +29,3 @@ public class Conta {
         
     }
 }
- 
