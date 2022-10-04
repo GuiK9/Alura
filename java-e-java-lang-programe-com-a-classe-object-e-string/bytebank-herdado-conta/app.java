b4553540f8146e86b09cc0f879c3dcc59568150c
GuiK9 <guilherme.tec1107@gmail.com>
2022-10-04 10:24:56 -0300

    name of directory chenged

--- java-e-java-lang-programe-com-a-classe-object-e-string/bytebank-herdado-conta/app.java
+++ java-e-java-lang-programe-com-a-classe-object-e-string/bytebank-herdado-conta/app.java
@@ -1,5 +1,5 @@
 public class app {
     public static void main(String[] args) {
-        System.out.println("aaaaaaaaaaaaaaaaaaaaaaa4444444444444444");
+        System.out.println("qqwqwqeqweqweqwe");
     }
 }
