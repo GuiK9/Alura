4545b46a75465048f8375f5c359649ecb210fcbe
GuiK9 <guilherme.tec1107@gmail.com>
2022-10-18 17:32:03 -0300

    class

---
+++ java-io/src/TesteLeitura2.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TesteLeitura2 {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(new File("contas.csv"));
+
+        while(scanner.hasNextLine()){
+            String linha = scanner.nextLine();
+//            System.out.println(linha);
+
+            Scanner linhaScanner = new Scanner(linha);
+            linhaScanner.useLocale(Locale.US);
+            linhaScanner.useDelimiter(",");
+
+            String tipoConta = linhaScanner.next();
+            int agencia = linhaScanner.nextInt();
+            int numero = linhaScanner.nextInt();
+            String titular = linhaScanner.next();
+            double saldo = linhaScanner.nextDouble();
+
+            System.out.format(Locale.getDefault(),"Tipo:%s; Agência:%08d; Número:%d; Titular:%s; Saldo:%08.2f %n", tipoConta, agencia, numero, titular, saldo);
+
+//            String[] valores = linha.split(",");
+//            System.out.println(valores[3]);
+        }
+
+        scanner.close();
+        scanner.close();
+
+    }
+
+}
