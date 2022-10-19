dc1664c18ae8023abfb6345d660489b883c55347
GuiK9 <guilherme.tec1107@gmail.com>
2022-10-19 17:25:23 -0300

    classes

---
+++ java-io/input-output/src/TesteSerilizacao.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class TesteSerilizacao {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        String nome = "Guilherme Sousa";
+
+//        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("objeto.bin"));
+//        oos.writeObject(nome);
+
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("objeto.bin"));
+        String oisreturn = (String) ois.readObject();
+        System.out.println(oisreturn);
+
+        ois.close();
+//        oos.close();
+    }
+}
