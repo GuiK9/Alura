e8e19c24cd468906568f4318b797b110134048f7
GuiK9 <guilherme.tec1107@gmail.com>
2022-10-26 17:27:07 -0300

    TestandoListas.java - aula1

---
+++ java-collections/gerenciador-de-cursos/src/TestandoListas.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class TestandoListas {
+    public static void main(String[] args) {
+        String aula1 = "Conhecendo mais de listas";
+        String aula2 = "Modelando a classe Alura";
+        String aula3 = "rabalhando com Cursos e Sets";
+
+        ArrayList<String> aulas = new ArrayList<>();
+
+        aulas.add(aula1);
+        aulas.add(aula2);
+        aulas.add(aula3);
+
+        System.out.println(aulas);
+
+        aulas.remove(0);
+
+        System.out.println(aulas);
+
+        for (String aula: aulas) {
+            System.out.println("Aula: " + aula + ";");
+        }
+
+        String primeiraAula = aulas.get(0);
+
+        System.out.println("A primeira aula é: " + primeiraAula);
+
+        for (int i = 0; i < aulas.size(); i++) {
+            System.out.println("aula" + (i + 1) + "°: " + aulas.get(i));
+
+        }
+
+
+
+    }
+}
