ed5f254b4ad8a3ca547a3e6f25f9268e3dfdf36d
GuiK9 <guilherme.tec1107@gmail.com>
2022-10-27 17:34:59 -0300

    classes

--- java-collections/gerenciador-de-cursos/src/TestandoListas.java
+++ java-collections/gerenciador-de-cursos/src/TestandoListas.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class TestandoListas {
     public static void main(String[] args) {
@@ -12,26 +13,29 @@ public class TestandoListas {
         aulas.add(aula2);
         aulas.add(aula3);
 
-        System.out.println(aulas);
-
+//        System.out.println(aulas);
         aulas.remove(0);
+//        System.out.println(aulas);
 
+//        for (String aula: aulas) {
+//            System.out.println("Aula: " + aula + ";");
+//        }
+
+//        String primeiraAula = aulas.get(0);
+//        System.out.println("A primeira aula é: " + primeiraAula);
+
+//        for (int i = 0; i < aulas.size(); i++) {
+//            System.out.println("aula" + (i + 1) + "°: " + aulas.get(i));
+//        }
+
+        aulas.forEach(aula -> {
+            System.out.println("percorrendo: ");
+            System.out.println("Aula " + aula);
+        });
+
+        aulas.add("Aumentando o nosso conhecimento");
+        Collections.sort(aulas);
         System.out.println(aulas);
-
-        for (String aula: aulas) {
-            System.out.println("Aula: " + aula + ";");
-        }
-
-        String primeiraAula = aulas.get(0);
-
-        System.out.println("A primeira aula é: " + primeiraAula);
-
-        for (int i = 0; i < aulas.size(); i++) {
-            System.out.println("aula" + (i + 1) + "°: " + aulas.get(i));
-
-        }
-
-
 
     }
 }
