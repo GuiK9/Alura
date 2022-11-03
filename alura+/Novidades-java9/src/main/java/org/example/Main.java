2793a9a50d3df5c1e524b8fffb0d0e355bf5613a
GuiK9 <guilherme.tec1107@gmail.com>
2022-11-03 16:24:56 -0300

    classes

---
+++ alura+/Novidades-java9/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<String> nomes = List.of("nome1", "nome2", "nome3");
+
+        
+
+    }
+}
