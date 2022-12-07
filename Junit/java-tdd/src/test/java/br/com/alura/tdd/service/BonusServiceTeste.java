556b2a8481e9ff175426bf440c8fd9c74e4e5afd
GuiK9 <guilherme.tec1107@gmail.com>
2022-12-07 19:55:30 -0300

    correção bônus service, funcionário com alto salário agora devolve uma exception

--- Junit/java-tdd/src/test/java/br/com/alura/tdd/service/BonusServiceTeste.java
+++ Junit/java-tdd/src/test/java/br/com/alura/tdd/service/BonusServiceTeste.java
@@ -11,8 +11,9 @@ public class BonusServiceTeste {
     @Test
     void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
         BonusService service = new BonusService();
-        BigDecimal bonus = service.calcularBonus(new Funcionario("Marcelo Azevedo De Paula", LocalDate.now(), new BigDecimal("12000")));
-        Assertions.assertEquals(new BigDecimal(0), bonus);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            BigDecimal bonus = service.calcularBonus(new Funcionario("Marcelo Azevedo De Paula", LocalDate.now(), new BigDecimal("12000")));
+        });
     }
 
     @Test
@@ -26,7 +27,11 @@ public class BonusServiceTeste {
     @Test
     void bonusDeveriaSerZeroParaFuncionarioComMilDeSalario() {
         BonusService service = new BonusService();
-        BigDecimal bonus = service.calcularBonus(new Funcionario("Marcelo Maria", LocalDate.now(), new BigDecimal("10000")));
-        Assertions.assertEquals(new BigDecimal("0"), bonus);
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            BigDecimal bonus = service.calcularBonus(new Funcionario("Marcelo Maria", LocalDate.now(), new BigDecimal("10000")));
+        });
+
+
     }
 }
