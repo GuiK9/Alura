766c2b3789acac33c088a0e22e1c2d70c5756613
GuiK9 <guilherme.tec1107@gmail.com>
2022-12-19 17:55:31 -0300

    classes

--- java-9-14/HTTP-2/src/main/java/org/example/dao/PlanetaDAO.java
+++ java-9-14/HTTP-2/src/main/java/org/example/dao/PlanetaDAO.java
@@ -1,22 +1,24 @@
 package org.example.dao;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.concurrent.CompletableFuture;
 
 public class PlanetaDAO {
 
-	public void listar() throws URISyntaxException, IOException, InterruptedException {
+	public void listar() throws URISyntaxException {
 		HttpClient httpClient = HttpClient.newBuilder().build();
 
-		HttpResponse<String> send = httpClient.send(
+		CompletableFuture<HttpResponse<String>> resp = httpClient.sendAsync(
 				HttpRequest.newBuilder().uri(new URI("http://localhost:8080/planetas")).GET().build(),
-				BodyHandlers.ofString());
+				BodyHandlers.ofString())
+				.whenComplete((s, t) -> {
+					System.out.println(s.body());
+				});
 
-		System.out.println(send.body());
 	}
 }
