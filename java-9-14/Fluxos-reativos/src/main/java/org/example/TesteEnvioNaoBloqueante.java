d9dbb29fbe7074aef22d6b6bcda64e2f1c9d521e
GuiK9 <guilherme.tec1107@gmail.com>
2022-12-21 05:00:59 -0300

    classes

--- java-9-14/Fluxos-reativos/src/main/java/org/example/TesteEnvioNaoBloqueante.java
+++ java-9-14/Fluxos-reativos/src/main/java/org/example/TesteEnvioNaoBloqueante.java
@@ -5,6 +5,8 @@ import org.example.wsclient.NotaFiscalWSClient;
 
 import java.time.LocalDate;
 import java.util.Scanner;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 import java.util.concurrent.SubmissionPublisher;
 
 
@@ -12,10 +14,17 @@ import java.util.concurrent.SubmissionPublisher;
 public class TesteEnvioNaoBloqueante {
 
 	public static void main(String[] args) {
+		Executor newFixedThreadPool = Executors.newFixedThreadPool(3);
 		NotaFiscal primeiraNotaFiscal = new NotaFiscal("Jo�o", 39.99, LocalDate.now());
-		SubmissionPublisher<NotaFiscal> publisher = new SubmissionPublisher<>();
+		SubmissionPublisher<NotaFiscal> publisher = new SubmissionPublisher<>(newFixedThreadPool, 1);
 		NotaFiscalWSClient nfwsc = new NotaFiscalWSClient();
 		publisher.consume(nfwsc::enviar);
+		publisher.consume(data -> {
+			System.out.println("A thread do TesteEnvioNãaBloquante é: " + Thread.currentThread().getName());
+		});
+		publisher.consume(data -> {
+			System.out.println("A thread do TesteEnvioNãaBloquante é: " + Thread.currentThread().getName());
+		});
 		publisher.submit(primeiraNotaFiscal);
 		System.out.println("Voc� ir� receber a nota fiscal no seu e-mail");
 		Scanner scan = new Scanner(System.in);
