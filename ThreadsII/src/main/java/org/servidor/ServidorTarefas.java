0c20a39f76266dbf5e86d20e06ccdd66afff0d3b
GuiK9 <guilherme.tec1107@gmail.com>
2023-01-03 10:25:06 -0300

    classes

--- ThreadsII/src/main/java/org/servidor/ServidorTarefas.java
+++ ThreadsII/src/main/java/org/servidor/ServidorTarefas.java
@@ -2,18 +2,20 @@ package org.servidor;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ServidorTarefas {
     public static void main(String[] args) throws Exception {
         System.out.println("----- iniciando o servidor -----");
         ServerSocket servidor = new ServerSocket(12345);
 
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+
         while(true){
             Socket socket = servidor.accept();
-
             DistribuirTarefas distribuirTarefas = new DistribuirTarefas(socket);
-            Thread threadCliente = new Thread(distribuirTarefas);
-            threadCliente.start();
+            threadPool.execute(distribuirTarefas);
         }
     }
 }
