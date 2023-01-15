90e8711e3df1e665d55d3128ec1e2db6cd9490c3
GuiK9 <guilherme.tec1107@gmail.com>
2023-01-15 14:08:42 -0300

    classes

--- ThreadsII/src/main/java/org/servidor/ServidorTarefas.java
+++ ThreadsII/src/main/java/org/servidor/ServidorTarefas.java
@@ -3,19 +3,21 @@ package org.servidor;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ServidorTarefas {
     private final ServerSocket servidor;
     private final ExecutorService threadPool;
-    private boolean estaRodando;
+    private AtomicBoolean estaRodando;
 
     public ServidorTarefas() throws IOException {
         System.out.println("----- iniciando o servidor -----");
         this.servidor = new ServerSocket(12345);
         this.threadPool = Executors.newCachedThreadPool();
-        this.estaRodando = true;
+        this.estaRodando = new AtomicBoolean(true);
     }
     public static void main(String[] args) throws Exception {
         ServidorTarefas servidor = new ServidorTarefas();
@@ -23,15 +25,20 @@ public class ServidorTarefas {
         servidor.parar();
     }
     public void rodar() throws IOException {
-        while(this.estaRodando){
-            Socket socket = servidor.accept();
-            DistribuirTarefas distribuirTarefas = new DistribuirTarefas(socket, this);
-            threadPool.execute(distribuirTarefas);
+        while(this.estaRodando.get()){
+            try {
+                Socket socket = servidor.accept();
+                DistribuirTarefas distribuirTarefas = new DistribuirTarefas(socket, this);
+                threadPool.execute(distribuirTarefas);
+            } catch (SocketException e) {
+                System.out.println("SocketException, está rodando? " + this.estaRodando);
+            }
         }
     }
     public void parar() throws IOException {
-        estaRodando = false;
+        this.estaRodando.set(false);
         servidor.close();
         threadPool.shutdown();
+//      System.exit(0);
     }
 }
