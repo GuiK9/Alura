65da59f7a4d4a7dd3a574d9f85e0d0bb32910b5c
GuiK9 <guilherme.tec1107@gmail.com>
2023-01-23 15:13:09 -0300

    classes

--- ThreadsII/src/main/java/org/projeto1/servidor/TarefaConsumir.java
+++ ThreadsII/src/main/java/org/projeto1/servidor/TarefaConsumir.java
@@ -12,11 +12,12 @@ public class TarefaConsumir implements Runnable{
     @Override
     public void run() {
         try {
-            String comando = filaComandos.take();
+            String comando;
+            while((comando = filaComandos.take()) != null){
+                System.out.println("Consumindo comando " + comando + ", " +Thread.currentThread().getName());
+                Thread.sleep(10000);
+            }
 
-            System.out.println("Consumindo comando " + comando);
-            Thread.sleep(10000);
-            System.out.println("consumido!!!!!!");
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
