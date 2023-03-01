ab1fe17b18ea78dd87db7e6767dcec32ac4bfbd6
GuiK9 <guilherme.tec1107@gmail.com>
2023-03-01 14:42:48 -0300

    article

--- Java-JDBC/src/main/java/org/example/Main.java
+++ Java-JDBC/src/main/java/org/example/Main.java
@@ -10,10 +10,14 @@ public class Main {
             Connection con = DriverManager.getConnection(stringConexcao, usuario, senha);
             Statement stmt = con.createStatement();
             String sql = "SELECT * FROM playground";
+            String sql2 =  "INSERT INTO playground (type, color, location, install_date) VALUES ('lock',  'red', 'north', '2015-03-12')";
             ResultSet result = stmt.executeQuery(sql);
             while(result.next()) {
                 System.out.println(result.getString("type"));
             }
+
+            int resultUpdate = stmt.executeUpdate(sql2);
+            System.out.println(resultUpdate);
         } catch (Exception e) {
             throw new RuntimeException(e);
         };
