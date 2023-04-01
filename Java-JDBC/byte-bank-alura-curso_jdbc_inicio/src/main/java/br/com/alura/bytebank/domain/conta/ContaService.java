f722d6b6fb4b08db280c29aa2a7ab8ec3cb3ab96
GuiK9 <guilherme.tec1107@gmail.com>
2023-04-01 14:56:12 -0300

    classes

--- Java-JDBC/byte-bank-alura-curso_jdbc_inicio/src/main/java/br/com/alura/bytebank/domain/conta/ContaService.java
+++ Java-JDBC/byte-bank-alura-curso_jdbc_inicio/src/main/java/br/com/alura/bytebank/domain/conta/ContaService.java
@@ -21,7 +21,8 @@ public class ContaService {
     private Set<Conta> contas = new HashSet<>();
 
     public Set<Conta> listarContasAbertas() {
-        return contas;
+        Connection conn = connection.recuperarConexao();
+        return new ContaDAO(conn).listar();
     }
 
     public BigDecimal consultarSaldo(Integer numeroDaConta) {
@@ -30,33 +31,8 @@ public class ContaService {
     }
 
     public void abrir(DadosAberturaConta dadosDaConta) {
-        var cliente = new Cliente(dadosDaConta.dadosCliente());
-        var conta = new Conta(dadosDaConta.numero(), cliente);
-        if (contas.contains(conta)) {
-            throw new RegraDeNegocioException("Já existe outra conta aberta com o mesmo número!");
-        }
-
-        String sql = "INSET INTO conta (numero, saldo, cliente_nome, cliente_cpf, cliente_email) VALUES (?, ?, ?, ?, ?)";
-
         Connection conn = connection.recuperarConexao();
-
-        PreparedStatement preparedStatement = null;
-
-        try {
-            preparedStatement = conn.prepareStatement(sql);
-            preparedStatement.setInt(1, conta.getNumero());
-            preparedStatement.setBigDecimal(2, conta.getSaldo());
-            preparedStatement.setString(3, dadosDaConta.dadosCliente().nome());
-            preparedStatement.setString(4, dadosDaConta.dadosCliente().cpf());
-            preparedStatement.setString(5, dadosDaConta.dadosCliente().email());
-
-            preparedStatement.execute();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-
-
+        new ContaDAO(conn).salvar(dadosDaConta);
     }
 
     public void realizarSaque(Integer numeroDaConta, BigDecimal valor) {
