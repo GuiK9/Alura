d42d8cb9b76bdb2feca3a1642de907cfaec76837
GuiK9 <guilherme.tec1107@gmail.com>
2023-05-13 15:21:55 -0300

    classes

--- Java-Spring/api/src/main/java/med/voll/api/Controller/MdedicoController.java
+++ Java-Spring/api/src/main/java/med/voll/api/Controller/MdedicoController.java
@@ -1,5 +1,6 @@
 package med.voll.api.Controller;
 
+import med.voll.api.medico.DadosCadastroMedico;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("medicos")
 public class MdedicoController {
     @PostMapping
-    public void cadastrar(@RequestBody String JSON) {
-        System.out.println(JSON);
+    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
+        System.out.println(dados);
     }
 }
