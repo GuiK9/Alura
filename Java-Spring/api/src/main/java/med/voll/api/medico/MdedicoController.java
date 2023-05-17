5850b1f1e707eef7a209d268b0d0eea6fa44d8b4
GuiK9 <guilherme.tec1107@gmail.com>
2023-05-17 14:09:07 -0300

    método de listagem

--- Java-Spring/api/src/main/java/med/voll/api/medico/MdedicoController.java
+++ Java-Spring/api/src/main/java/med/voll/api/medico/MdedicoController.java
@@ -1,15 +1,12 @@
-package med.voll.api.Controller;
+package med.voll.api.medico;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.Controller.DadosListagemMedico;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -22,4 +19,10 @@ public class MdedicoController {
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
     }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar(){
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    }
+
 }
