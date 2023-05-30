6ccfc57ca23ee4b2d9d44bc84c9f790b20201f55
GuiK9 <guilherme.tec1107@gmail.com>
2023-05-30 14:17:02 -0300

    alterações em nomes de pacotes

--- Java-Spring/api/src/main/java/med/voll/api/configuration/CorsConfiguration.java
+++ Java-Spring/api/src/main/java/med/voll/api/configuration/CorsConfiguration.java
@@ -1,4 +1,4 @@
-package med.voll.api.Configuration;
+package med.voll.api.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
