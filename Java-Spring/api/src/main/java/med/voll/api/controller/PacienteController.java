1fc045e4ad59b3a29d2a0d2c2eb842a8696f97e9
GuiK9 <guilherme.tec1107@gmail.com>
2023-07-25 10:06:38 -0300

    classes

--- Java-Spring/api/src/main/java/med/voll/api/controller/PacienteController.java
+++ Java-Spring/api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,5 +1,7 @@
 package med.voll.api.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.domain.paciente.*;
@@ -15,6 +17,7 @@ import java.net.URI;
 
 @RestController
 @RequestMapping("pacientes")
+@SecurityRequirement(name = "bearer-key")
 public class PacienteController {
     @Autowired
     private PacienteRepository repository;
