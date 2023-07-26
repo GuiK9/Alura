b2ea3a14bf740b5094d84a06b1094866a1673286
GuiK9 <guilherme.tec1107@gmail.com>
2023-07-26 09:52:13 -0300

    pré-build

--- Java-Spring/api/src/main/java/med/voll/api/domain/consulta/DadosDetalhamentoConsulta.java
+++ Java-Spring/api/src/main/java/med/voll/api/domain/consulta/DadosDetalhamentoConsulta.java
@@ -2,12 +2,13 @@ package med.voll.api.domain.consulta;
 
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
+import med.voll.api.domain.medico.Especialidade;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public record DadosDetalhamentoConsulta( Long id, Long idMedico, Long idPaciente, LocalDateTime data) {
+public record DadosDetalhamentoConsulta(Long id, Long idMedico, Long idPaciente, LocalDateTime data, Especialidade especialidade) {
     public DadosDetalhamentoConsulta(Consulta consulta) {
-        this(consulta.getId(), consulta.getMedico().getId(), consulta.getPaciente().getId(), consulta.getData());
+        this(consulta.getId(), consulta.getMedico().getId(), consulta.getPaciente().getId(), consulta.getData(), consulta.getMedico().getEspecialidade());
     }
 }
